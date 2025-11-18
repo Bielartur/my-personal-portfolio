66f410b5a569ea7426d353b638130d406c146ba4
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-11-18 12:55:31 -0300

    Adding the Theme Toggle

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+
+export function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
